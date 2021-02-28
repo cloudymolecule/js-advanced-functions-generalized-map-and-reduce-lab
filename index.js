514e6d911eec4044299a65d72ccce160e79ba41d
Cloudymolecule <baires82@gmail.com>
2021-02-28 10:15:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, myCallback){
+    let newArray = []
+    array.forEach(element => {
+       newArray.push(myCallback(element))
+    })
+    return newArray
+}
+
+
+function reduce(array, myCallback, startingPoint){
+    let total = 0
+    let result = array.every(Boolean)
+    if (startingPoint) {total = startingPoint}
+    array.forEach(element => {
+        total = myCallback(total, element)
+    })
+    if (total) {return total} else {return result}
+}
